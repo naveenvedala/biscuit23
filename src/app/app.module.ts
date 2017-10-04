019364471132d087753ad85950d15acca30ef270
NaveenVedala23 <vedala.naveen23@gmail.com>
2017-10-04 12:07:00 +0530

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+import { LoginBaseComponent } from './login-base/login-base.component';
+import {
+  MatInputModule, MdAutocompleteModule, MdButtonModule, MdButtonToggleModule,
+  MdCardModule, MdCheckboxModule, MdChipsModule, MdDatepickerModule, MdDialogModule,
+  MdExpansionModule, MdGridListModule, MdIconModule, MdInputModule, MdListModule,
+  MdMenuModule, MdNativeDateModule, MdPaginatorModule, MdProgressBarModule,
+  MdProgressSpinnerModule, MdRadioModule, MdRippleModule, MdSelectModule,
+  MdSidenavModule, MdSliderModule, MdSlideToggleModule, MdSnackBarModule,
+  MdSortModule, MdTableModule, MdTabsModule, MdToolbarModule, MdTooltipModule, MdStepperModule,
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routes';
+import { LoginComponent } from './login/login.component';
+import { ForgotComponent } from './forgot/forgot.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginBaseComponent,
+    LoginComponent,
+    ForgotComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,33 @@
+// import { Routes, RouterModule } from '@angular/router';
+//
+// const routes: Routes = [
+//   {
+//     path: '', component: './modules/app.module#LoginBaseComponent',
+//     children: [
+//       { path: '', component: './modules/app.module#LoginBaseComponent' }
+//     ]
+//   }
+// ];
+// export const appRoutingProviders: any[] = [];
+// export const routing = RouterModule.forRoot(routes);
+// //export const appRoutes: any = RouterModule.forRoot(routes, { useHash: true });
+import { Routes, RouterModule } from '@angular/router';
+import { LoginBaseComponent } from './login-base/login-base.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotComponent } from './forgot/forgot.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login', component: LoginBaseComponent,
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'forgot', component: ForgotComponent }
+    ]
+  },
+
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing = RouterModule.forRoot(routes);
